7acbcc6c8f28e24a655278c4de7ca6bc7429fadf
JaneHerrick <poporona@rambler.ru>
2023-05-21 23:36:31 +0300

    SB_PR15.4

--- SB_CPP_PR_14.4.cpp
+++ SB_CPP_PR_14.4.cpp
@@ -1,9 +1,9 @@
-#include <string>
-#include <iostream>
-using namespace std;
-
-int main()
-{
+//#include <string>
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
 
     /*Артем, объясните почему в коде, представленном ниже, из видеоурока, если использовать getline,
     то не ставиться пробел при ВЫВОДЕ данных. Конкретно - между именем и возрастом. Делала все как в уроке.
@@ -19,12 +19,12 @@ int main()
 
     //cout << name << " " << age << '\n';     // Вот тут подставленно - " ", но в уроке этого нет.
 
-    string quote = "Well planned - half done";
-
-    cout << quote << endl;
-    cout << quote.length() << endl;
-    cout << quote[0] << endl;
-    cout << quote.back() << endl;
-
-    return 0;
-}
+//    string quote = "Well planned - half done";
+//
+//    cout << quote << endl;
+//    cout << quote.length() << endl;
+//    cout << quote[0] << endl;
+//    cout << quote.back() << endl;
+//
+//    return 0;
+//}
